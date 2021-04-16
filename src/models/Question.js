8394aec766bb4ace3076c1c1354b3e0a68e8769f
Ferdz <ferheredia99@gmail.com>
2021-04-16 11:11:15 -0600

    first commit

---
+++ src/models/Question.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const QuestionSchema = new mongoose.Schema({
+            
+            response_code: Number,
+            results: []
+    }
+
+)
+
+module.exports = mongoose.model('Question', QuestionSchema)
+
+
+
+
